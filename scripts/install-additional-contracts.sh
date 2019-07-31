df6e8ebe05f5825b6d9c45da3393f02d9ad28816
wulfraem <wulfraem@users.noreply.github.com>
2019-07-31 10:29:45 +0200

    update additional contracts install script to log less
    - [CORE-352]

--- scripts/install-additional-contracts.sh
+++ scripts/install-additional-contracts.sh
@@ -2,12 +2,12 @@
 
 # oraclize base class
 rm -rf node_modules/ethereum-api
-git clone https://github.com/oraclize/ethereum-api.git -b solc_0.4.25-compatibility-patch node_modules/ethereum-api
+git clone --quiet https://github.com/oraclize/ethereum-api.git -b solc_0.4.25-compatibility-patch node_modules/ethereum-api
 
 # solidity-stringutils 
 rm -rf node_modules/solidity-stringutils
-git clone https://github.com/Arachnid/solidity-stringutils.git node_modules/solidity-stringutils
+git clone --quiet https://github.com/Arachnid/solidity-stringutils.git node_modules/solidity-stringutils
 cd node_modules/solidity-stringutils
 # no releases, tags or branches to address for latest stable, so use commit from 2018-07-25T12:49:44Z
-git checkout 3c63f18245645ba600cae2191deba7221512f753
+git checkout --quiet 3c63f18245645ba600cae2191deba7221512f753
 cd ..
